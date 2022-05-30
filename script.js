1d07923d655940e32dc9a9ca96c364be1c2f0391
Bernabe <bernabrb@gmail.com>
2022-05-30 15:36:30 -0300

    Librerias

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById('botonReglas').addEventListener('click', mostrarReglas)
+
+function mostrarReglas() {
+    console.log()
+    Swal.fire(
+        'REGLAS',
+        'Al hacer click en INICIAR JUEGO se mostrarán las cartas por unos segundos. Luego tenés que adivinar los pares. Por cada acierto sumás 3 puntos, por cada error restás 1. ¡Buena suerte!',
+        'question'
+      )
+}
